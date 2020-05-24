3f1257e5edf0f939749aed787e268f1a146520d1
SuvorkovRN <suvorkovRN@gmail.com>
2020-05-25 01:26:11 +0500

    v3

--- server.js
+++ server.js
@@ -61,8 +61,19 @@ app.use(router.routes()).use(router.allowedMethods());
 
 const msgArr = [];
 
-router.get('/msgArr', async (ctx, next) => {
-  ctx.response.body = msgArr;
+// router.get('/msgArr', async (ctx, next) => {
+//   ctx.response.body = JSON.stringify(msgArr);
+//   console.log('Message sended to frontend');
+// });
+
+router.get('/msgArr/:numb', async (ctx, next) => {
+  const indexOfLength = msgArr.length - ctx.params.numb;
+  let startIndex = 0;
+  if (indexOfLength - 10 > 0) {
+    startIndex = indexOfLength - 10;
+  }
+  const output = 
+  ctx.response.body = JSON.stringify(msgArr);
   console.log('Message sended to frontend');
 });
 
@@ -71,80 +82,20 @@ router.post('/msgArr', async (ctx, next) => {
   console.log('Message added');
   console.log(msgArr);
   ctx.response.status = 204
-
-
-  // test code
-  // const { file } = ctx.request.files;
-  // if (file) {
-  //   console.log('file');
-  //   console.log(file);
-  //   let id = uuid.v4();   
-  //   let codedImg = file.name;
-  //   const copyOnServer = new Promise((resolve, reject) => {
-  //     fs.copyFile(file.path, `${public}/${codedImg}`, (err) => {
-  //     if (err) {
-  //       reject(err);
-  //     } else {
-  //       console.log('copyFile work');
-  //       msgArr.push({ file: `${codedImg}`, id: id});
-  //       console.log(msgArr);
-  //       resolve(codedImg);
-  //       }
-  //     });
-  //   });
-
-  // } else {
-  //   msgArr.push({...ctx.request.body, id: uuid.v4()});
-  //   console.log('Message added');
-  //   console.log(msgArr);
-  //   ctx.response.status = 204
-  // }
-
-
-
-
 });
-
-router.post('/msgFiles', async (ctx, next) => {
-  const { file } = ctx.request.files;
-  console.log(file);
-  let id = uuid.v4();
-  // let fileType = file.name.match(/(?<=\.).+$/).toString();
-  // console.log(fileType);
-  // let codedImg = `${id}.${fileType}`;
-
-  let codedImg = file.name;
-
-  const copyOnServer = new Promise((resolve, reject) => {
-    fs.copyFile(file.path, `${public}/${codedImg}`, (err) => {
-    if (err) {
-      reject(err);
-    } else {
-      console.log('copyFile work');
-      msgArr.push({ file: `${codedImg}`, id: id});
-      console.log(msgArr);
-      resolve(codedImg);
-      }
-    });
-  });
-  ctx.response.status = 204;
-});
-
-// router.delete('/users/:name', async (ctx, next) => {
-//   const index = usersList.findIndex(({ name }) => name === ctx.params.name);
-//   if (index !== -1) {
-//     usersList.splice(index, 1);
-//   };
-//   ctx.response.status = 204
-// });
 
 wsServer.on('connection', (ws, req) => {
   ws.on('message', msg => {
+    // const newObj = {text: msg, id: uuid.v4()};
+    const newMessage = JSON.parse(msg);
+    newMessage.id = uuid.v4();
+    msgArr.push(newMessage);
+    // console.log(msgArr);
     [...wsServer.clients]
     .filter(o => {
       return o.readyState === WS.OPEN;
     })
-    .forEach(o => o.send(msg));
+    .forEach(o => o.send(JSON.stringify(newMessage)));
   });
   ws.on('close', msg => {
     [...wsServer.clients]
@@ -154,158 +105,5 @@ wsServer.on('connection', (ws, req) => {
     .forEach(o => o.send(JSON.stringify({type: 'delete'})));
     ws.close();
   });
-  [...wsServer.clients]
-    .filter(o => {
-      return o.readyState === WS.OPEN;
-    })
-    .forEach(o => o.send(JSON.stringify({type: 'add'})));
+ 
 });
-
-
-// app.use(router.routes()).use(router.allowedMethods());
-// const port = process.env.PORT || 7070;
-// server.listen(port);
-
-
-
-
-// const instances = [];
-
-// router.get('/instances', async (ctx, next) => {
-//   ctx.response.body = instances;
-//   console.log(instances);
-// });
-
-// router.post('/instances', async (ctx, next) => {
-//   const id = uuid.v4();
-//   let startMessage = JSON.stringify({
-//       type: 'server log',
-//       id: id,
-//       msg: 'Received "Create command"',
-//       date: new Date(),
-//     });
-//     [...wsServer.clients]
-//     .filter(o => {
-//       return o.readyState === WS.OPEN;
-//     })
-//     .forEach(o => o.send(startMessage));
-
-//   setTimeout(() => {
-//     instances.push({
-//       id,
-//       state: 'stopped',
-//     });
-//     let createMessage = JSON.stringify({
-//       type: 'server log',
-//       id: id,
-//       msg: 'Created',
-//       date: new Date(),
-//     });
-//     [...wsServer.clients]
-//     .filter(o => {
-//       return o.readyState === WS.OPEN;
-//     })
-//     .forEach(o => o.send(createMessage));
-//   }, 1000);
-
-//   ctx.response.body = {
-//     status: 'ok'
-//   };
-// });
-
-// router.patch('/instances/:id', async (ctx, next) => {
-//   let inputId = ctx.params.id;
-//   let changeMessage = JSON.stringify({
-//     type: 'server log',
-//     id: inputId,
-//     msg: 'Received "Change State"',
-//     date: new Date(),
-//   });
-//   [...wsServer.clients]
-//   .filter(o => {
-//     return o.readyState === WS.OPEN;
-//   })
-//   .forEach(o => o.send(changeMessage));
-
-//   const targetIndex = instances.findIndex((item) => item.id === inputId);
-//   if (targetIndex !== -1) {
-
-//     setTimeout(() => {
-//       let activeState = instances[targetIndex].state;
-//       if(activeState === 'stopped') {
-//         activeState = 'started';
-//       } else {
-//         activeState = 'stopped';
-//       }
-//       instances[targetIndex].state = activeState;
-
-//       let changeConfirm = JSON.stringify({
-//         type: 'server log',
-//         id: inputId,
-//         msg: activeState,
-//         date: new Date(),
-//       });
-//       [...wsServer.clients]
-//       .filter(o => {
-//         return o.readyState === WS.OPEN;
-//       })
-//       .forEach(o => o.send(changeConfirm));
-//     }, 1000);
-//   };
-//   ctx.response.body = {
-//     status: 'ok'
-//   }
-// });
-
-// router.delete('/instances/:id', async (ctx, next) => {
-//   const inputId = ctx.params.id;
-//   const targetIndex =  instances.findIndex((item) => item.id === inputId);
-//   if (targetIndex !== -1) {
-//     let deleteRequest = JSON.stringify({
-//       type: 'server log',
-//       id: inputId,
-//       msg: 'Received "Delete instance"',
-//       date: new Date(),
-//     });
-//     [...wsServer.clients]
-//     .filter(o => {
-//       return o.readyState === WS.OPEN;
-//     })
-//     .forEach(o => o.send(deleteRequest));
-
-//     setTimeout(() => {
-//       instances.splice(targetIndex, 1);
-//       let deleteConfirm = JSON.stringify({
-//         type: 'server log',
-//         id: inputId,
-//         msg: 'Deleted',
-//         date: new Date(),
-//       });
-//       [...wsServer.clients]
-//       .filter(o => {
-//         return o.readyState === WS.OPEN;
-//       })
-//       .forEach(o => o.send(deleteConfirm));
-//     }, 1000);
-//   };
-//   ctx.response.body = {
-//     status: 'ok'
-//   }
-// });
-
-// wsServer.on('connection', (ws, req) => {
-//   console.log('connection');
-//   ws.on('message', msg => {
-//     [...wsServer.clients]
-//     .filter(o => {
-//       return o.readyState === WS.OPEN;
-//     })
-//     .forEach(o => o.send(msg));
-//   });
-//   ws.on('close', msg => {
-//     console.log('close');
-//   });
-//   ws.on('change', msg => {
-//     console.log('change');
-//   });
-// });
